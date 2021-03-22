e8fa5b1963705e01fa95a9b7ceff4a82ebc50fad
Anurag Mishra <mail2anuragmishra@gmail.com>
2021-03-22 14:56:54 +0530

    d

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   docApiBaseUrl: 'https://api.docty.ai:3008/',
-  apiBaseUrl: 'https://apico.docty.ai/api/v1'
+  apiBaseUrl: 'https://apico.docty.ai/api/v1/'
 };
